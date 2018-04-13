0ad42e897d22fb7128956fb00b50161a2c7cf31b
tatianaab <abramovatatian@gmail.com>
2018-04-13 09:14:04 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  get 'friendships/index'
-  get 'friendships/create'
-  get 'friendships/update'
-  get 'friendships/destroy'
-  get 'sessions/index'
-  get 'user/index'
-  resources :user, only: [:new, :create, :show]
-  get 'welcome/index'
+  get '/users/search', to: 'users#search', as: 'search'
+  resources :users, only: [:new, :create, :show] do
+    resources :friendships, only: [:index, :create, :destroy, :update]
+  end
+  resources :sessions, only: [:new, :destroy]
+
+
+  get '/sessions/login', to: 'sessions#login', as: 'login'
+  post '/sessions/login', to: 'sessions#create'
+  get '/sessions/logout', to: 'sessions#destroy', as: "logout"
+  get '/posts/new', to: "posts#new"
+  get '/posts/:id/edit', to: "posts#edit"
+  delete '/posts/:id', to: "posts#destroy", as: "delete_post"
+  put 'posts/:id', to: 'posts#update'
+  post '/users/:id/posts', to: "posts#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
