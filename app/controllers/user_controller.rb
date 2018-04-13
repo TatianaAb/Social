0de2f34ca87204edb9767311671ce0154bdfaeeb
tatianaab <abramovatatian@gmail.com>
2018-04-13 08:41:14 -0400

    create friendships controller and model

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,36 @@
 class UserController < ApplicationController
   def index
   end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      @errors = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def search
+    @users = User.where('name LIKE ?', "%#{params[:search]}%").all
+    render '/users/search'
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    @posts = Post.where(receiver_id: @user.id)
+    @like = Like.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'friendships/index'
+  get 'friendships/create'
+  get 'friendships/update'
+  get 'friendships/destroy'
   get 'sessions/index'
   get 'user/index'
   resources :user, only: [:new, :create, :show]
